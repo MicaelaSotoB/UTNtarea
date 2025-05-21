bdafbe414c2f399c2dfd08d0fa2e945b5c94c0bb
Elias Soto <hmillhari.sj@gmail.com>
2025-05-21 10:27:39 -0300

    Primera parte de pacticas del While y parte de For

---
+++ ActividadFor/prac_For.py
@@ -0,0 +1,46 @@
+# Mostrar los números ascendentes desde el 1 al 10
+
+# for i in range(1,11):
+#     print(i)
+
+# Mostrar los números descendentes desde el 10 al 1
+# desc=10
+# for i in range(1, 11):
+#     print(desc)
+#     desc-=1
+
+
+# Ingresar un número. Mostrar los números desde 0 hasta el número ingresado.
+
+# num=int(input("Ingrese un numerito: "))
+# contador=0
+# for i in range(num+1):
+#     print(contador)
+#     contador+=1
+
+# Ingresar un número y mostrar la tabla de multiplicar de ese número. Por ejemplo si se ingresa el numero 5:
+
+# 	5 x 0 = 0
+# 	5 x 1  = 5
+# 	5 x 2 = 10
+# 	5 x 3  = 15 …
+
+# num=int(input("Ingrese un numero para mostrar su tabla: "))
+
+# for i in range(11):
+#     print(f"{num} x {i}={num*i}")
+
+
+# Se ingresan un máximo de 10 números o hasta que el usuario ingrese el número 0. 
+# Mostrar la suma y el promedio de todos los números.
+
+
+
+# Imprimir los números múltiplos de 3 entre el 1 y el 10.
+# Mostrar los números pares que hay desde la unidad hasta el número 50.
+# Realizar un programa que permita mostrar una pirámide de números. Por ejemplo: si se ingresa el numero 5, la salida del programa será la siguiente:
+
+
+# Ingresar un número. Mostrar todos los divisores que hay desde el 1 hasta el número ingresado. Mostrar la cantidad de divisores encontrados.
+# Ingresar un número. Determinar si el número es primo o no.
+# Ingresar un número. Mostrar cada número primo que hay entre el 1 y el número ingresado. Informar cuántos números primos se encontraron.
